cae423566a0b353340c71b605f220a1fcff7cab8
MAWOW1000 <duc22092003@gmail.com>
2024-11-03 15:33:39 +0700

    Duc commit update structure cide

--- src/ulti/mongoose.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    multiMongooseToObject(mongooses) {
-        return mongooses.map((mongoose) => mongoose.toObject());
-    },
-    mongooseToObject(mongoose) {
-        return mongoose.toObject();
-    },
-};
