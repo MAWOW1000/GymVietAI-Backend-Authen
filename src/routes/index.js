79e619f26c60f324b8f4b92c37755353f23c636c
MAWOW1000 <duc22092003@gmail.com>
2024-11-15 14:11:56 +0700

    Update structure

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const siteRouter = require('./site');
-const blogRouter = require('./blog');
-
-function route(app) {
-    app.use('/blog', blogRouter);
-    app.use('/', siteRouter);
-}
-
-module.exports = route;
